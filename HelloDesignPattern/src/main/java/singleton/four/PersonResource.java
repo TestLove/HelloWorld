e7351d8a04539498eb9ee56cdd6274fe1b77b742
testlove <1923161393@qq.com>
2021-09-12 11:39:48 +0800

    单例:枚举

---
+++ HelloDesignPattern/src/main/java/singleton/four/PersonResource.java
@@ -0,0 +1,17 @@
+package singleton.four;
+
+/**
+ * @author TestLove
+ * @version 1.0
+ * @date 2021/8/17 15:15
+ * @Description: null
+ */
+public enum PersonResource {
+    /**
+     ( PersonResource单例
+     */
+    personResource;
+    public void setPersonResource(){
+
+    }
+}
